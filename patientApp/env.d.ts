7cf355b372cad663220773ef522d8abbba1394d9
FridaK-sys <frida.karlsen@hotmail.com>
2025-01-31 11:16:39 +0100

    started connecting patientApp with backend

---
+++ patientApp/env.d.ts
@@ -0,0 +1,8 @@
+declare module '@env' {
+    export const CONTRACT_ADDRESS: string;
+    export const OWNER_ADDRESS: string;
+    export const RPC_URL: string;
+    export const OWNER_PRIVATE_KEY: string;
+    export const OTHER_PRIVATE_KEY: string;
+    export const OTHER_ADDRESS: string;
+}
--- patientApp/screens/Endringslogg.tsx
+++ patientApp/screens/Endringslogg.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
 
 import Header from "../components/Header";
 import Icon from "react-native-vector-icons/SimpleLineIcons";
+import { getAccessList, revokeAccess } from "../services/BlockchainService";
+
+import { OTHER_ADDRESS, OTHER_PRIVATE_KEY, OWNER_PRIVATE_KEY } from '@env';
+
 
 const Endringslogg = () => {
+  const [accessList, setAccessList] = useState<string[]>([]);
+
+  // const patientAddress: string = process.env.OWNER_ADDRESS || "";
+  // const workerPrivateKey: string = process.env.OTHER_PRIVATE_KEY || "";
+
+
+  useEffect(() => {
+    fetchAccessList();
+  }, []);
+
+  const fetchAccessList = async () => {
+    try {
+      const list = await getAccessList();
+      setAccessList(list);
+      console.log(list);
+    } catch (error) {
+      console.error("Error fetching access list:", error);
+    }
+  };
+
+  const handleRemoveAccess = async () => {
+    if (!accessList.includes(OTHER_ADDRESS)) {
+      console.log("Error", "User does not have access.");
+      return;
+    }
+
+    try {
+      await revokeAccess(OTHER_ADDRESS, OWNER_PRIVATE_KEY);
+      console.log("Success", "Access revoked successfully.");
+      fetchAccessList(); // Refresh access list
+    } catch (error) {
+      console.error("Error revoking access:", error);
+      console.log("Error", "Failed to revoke access.");
+    }
+  };
   const tableData = [
     ["Data", "Behandling", "Dokument"],
     [
@@ -39,6 +78,9 @@ const Endringslogg = () => {
           </View>
         ))}
       </View>
+      <TouchableOpacity style={styles.button} onPress={handleRemoveAccess}>
+        <Text style={styles.buttonText}>Fjern innsynsrettigheter</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -46,6 +88,19 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     alignItems: "center",
+  },
+  button: {
+    height: 100,
+    width: "90%",
+    marginTop: 40,
+    backgroundColor: "#0D9276",
+    borderRadius: 20,
+    alignItems: "center", // Centers text horizontally
+    justifyContent: "center", // Centers text vertically
+  },
+  buttonText: {
+    fontFamily: '"Times New Roman", Times, serif',
+    fontSize: 30,
   },
   table: {
     marginTop: 20,
