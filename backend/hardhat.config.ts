70ac1c63104507de22fba81954bf3c5ac098ac4a
Juni <junitrigger@gmail.com>
2025-02-10 12:02:01 +0100

    35: added infura setup

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -31,3 +31,19 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
+
+// For infura setup
+/*
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : [],
+    },
+  },
+};
+*/
